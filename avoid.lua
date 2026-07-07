c5e28d147c0dd665df3c8ba8336a8169ede186a7
fivecarsword <jengho200@naver.com>
2026-07-07 15:18:01 +0900

    .

--- avoid.lua
+++ avoid.lua
@@ -1,3 +1,5 @@
+// wget https://raw.githubusercontent.com/fivecarsword/cc-tweaked/master/avoid.lua
+
 local function gameLogic()
     if (relay.getInput("left")) then
         x = math.max(1, x - 1)
@@ -10,13 +12,15 @@ local function gameLogic()
     o_y = o_y + 1
 
     if (x == o_x and y == o_y) then
-        break
+        return false
     end
 
     if (o_y > height) then
         o_y = 0
         o_x = math.random(1, width)
     end
+
+    return true
 end
 
 local function draw()
@@ -30,11 +34,21 @@ local function draw()
 end
 
 local function music()
+    if (beat[beatIdx][1] <= beatFrame) then
+        beatIdx = (beatIdx + 1) #beat
+        beatFrame = 0
+    end
 
+    if (beatFrame == 0) then
+        speak.playNote(beat[beatIdx][0], 1, 12)
+    end
+
+    beatFrame = beatFrame + 1
 end
 
 mon = peripheral.find("monitor")
 relay = peripheral.find("redstone_relay")
+speak = peripheral.find("speaker")
 
 mon.setTextSale(3)
 
@@ -46,9 +60,34 @@ y = height
 o_x = math.random(1, width)
 o_y = 0
 
+melody = {
+
+}
+beat = {
+    {"basedrum", 5}
+}
+
+melodyIdx = 0
+melodyFrame = 0
+
+beatIdx = 0
+beatFrame = 0
+
+frame = 0
+
 while (true) do
-    gameLogic()
+    if (frame % 2 == 0) then
+        if (not gameLogic(frame)) then
+            break
+        end
+    end
+
     draw()
 
-    os.sleep(0.1)
+    music()
+
+    frame = frame + 1
+    os.sleep(0.05)
 end
+
+mon.clear()
